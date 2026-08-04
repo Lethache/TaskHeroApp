2cc92ea115cf724ebe68c9f71300f2aa84bad05f
Lethache <mishalol8474@gmail.com>
2026-08-04 00:19:37 -0400

    home sceen added

--- TaskHero/Views/HomeScreen.xaml.cs
+++ TaskHero/Views/HomeScreen.xaml.cs
@@ -6,4 +6,22 @@ public partial class HomeScreen : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void OnSetUpTaskClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(ChildAddingPage));
+    }
+
+    private void OnSetUpTaskClicked(object sender, TappedEventArgs e)
+    {
+
+    }
+    private void OnGiveaGiftClicked(object sender, TappedEventArgs e)
+    {
+        
+    }
+    private void OnCheckDoneTaskClicked(object sender, TappedEventArgs e)
+    {
+
+    }
 }
